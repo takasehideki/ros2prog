f9840a66a544370a258b48101cd9effa60d96837
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2024-01-06 22:00:07 +0900

    step4: implement turtle_publisher

--- pubsub_turtle/src/publisher.cpp
+++ pubsub_turtle/src/publisher.cpp
@@ -2,32 +2,79 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+/* Twist型のインクルード */
+#include "geometry_msgs/msg/twist.hpp"
 
+/* キー入力cin用 */
+using namespace std;
 using namespace std::chrono_literals;
 
 class Publisher : public rclcpp::Node
 {
 public:
   Publisher()
-  : Node("simple_publisher"), count_(0)
+  : Node("turtle_publisher")
   {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("chatter", 10);
+      /* Twist型の turtle1/cmd_vel をtopicとして出版ノードを生成 */
+    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10);
+    /* コールバック関数をタイマイベントとして登録 */
+    /* キー入力待ちでタイマのスケジューラが破綻するため，本来はRate.sleepで実現すべき */
     timer_ = this->create_wall_timer(
       500ms, std::bind(&Publisher::timer_callback, this));
   }
 
 private:
+  /* コールバック関数の定義 */
   void timer_callback()
   {
-    auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    publisher_->publish(message);
+    /* キーマップの表示 */
+    /* h-左 j-前 k-後 l-右 u-停止 */
+    RCLCPP_INFO(this->get_logger(), "Enter Key(+Enter) as Vimmer");
+    RCLCPP_INFO(this->get_logger(), "j:forward, k:backward, h:left, l:right");
+
+    /* 出版用のTwist型変数の宣言 */
+    geometry_msgs::msg::Twist vel;
+
+
+    /* キー入力を受け付ける */
+    char key;
+    cin >> key;
+
+    /* キー入力に応じて linear or angular を設定する */
+    switch(key)
+    {
+      case 'j': /* forward */
+        RCLCPP_INFO(this->get_logger(), "forwarding,,,");
+        vel.linear.x = 2.0;
+        break;
+      case 'k': /* backward */
+        RCLCPP_INFO(this->get_logger(), "backwarding,,,");
+        vel.linear.x = -2.0;
+        break;
+      case 'h': /* left */
+        RCLCPP_INFO(this->get_logger(), "turning left,,,");
+        vel.angular.z = 1.0;
+        break;
+      case 'l': /* right */
+        RCLCPP_INFO(this->get_logger(), "turning right,,,");
+        vel.angular.z = -1.0;
+        break;
+      default:  /* boo! */
+        RCLCPP_INFO(this->get_logger(), "Input key is invalid");
+        RCLCPP_INFO(this->get_logger(), "Are you really Vimmer?");
+        vel.linear.x = 0.0;
+        vel.angular.z = 0.0;
+        break;
+    }
+    /* 出版値のログ出力 */
+    RCLCPP_INFO(this->get_logger(),
+        "Publishing: linear = %.2lf angular = %.2lf",
+        vel.linear.x, vel.angular.z);
+    /* turtle1/cmd_vel に出版 */
+    publisher_->publish(vel);
   }
   rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  size_t count_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
 };
 
 int main(int argc, char * argv[])
